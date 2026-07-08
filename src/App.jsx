95d05e77e496aa11814306dd5a54240d80ffb1ab
Nitinjr812 <nitinjr812@gmail.com>
2026-07-08 23:14:40 +0530

    email verify otp generate

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,10 @@ function AppInner() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-
+      <Route path="/verify-email/:token" element={<VerifyEmail />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/*" element={
+
         <ProtectedRoute>
           <div className="min-h-screen" style={{ background: t.bgPage }}>
             <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
@@ -63,8 +65,7 @@ function AppInner() {
                 <Route path="/notifications" element={<Notifications />} />
                 <Route path="/settings" element={<Settings />} />
                 <Route path="/suppliers" element={<Suppliers />} />
-                <Route path="/verify-email/:token" element={<VerifyEmail />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
+
                 <Route path="*" element={<Navigate to="/dashboard" replace />} />
               </Routes>
             </main>
